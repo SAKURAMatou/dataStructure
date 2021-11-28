69113fd4ae07c83e9d68f04f3b7de1476b5da43c
“DML” <dailin1357@gmail.com>
2021-11-28 22:04:05 +0800

    简单题目练习结束

--- src/main/java/com/dl/nowcoder/mathTest/MathSolution.java
+++ src/main/java/com/dl/nowcoder/mathTest/MathSolution.java
@@ -1,6 +1,8 @@
 package com.dl.nowcoder.mathTest;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class MathSolution {
@@ -33,8 +35,47 @@ public class MathSolution {
         System.out.println(Arrays.stream(arr).mapToObj(i -> String.valueOf(i)).collect(Collectors.joining("+")));
     }
 
+    /**
+     * HJ86 求最大连续bit数
+     * 求一个int类型数字对应的二进制数字中1的最大连续数，例如3的二进制为00000011，最大连续2个1
+     *
+     * @param a
+     * @return
+     */
+    public int getMaxByteCount(int a) {
+        //使用Javaapi转化为二进制字符串
+        String str = Integer.toBinaryString(a);
+        //求出二进制字符串中连续的1的数量;
+        //字符串中只包含了0和1，通过0分割字符串；找出分割到的最长子串的长度
+        Optional<String> max = Arrays.stream(str.split("0")).max(Comparator.comparing(String::length));
+        if (max.isPresent()) {
+            return max.get().length();
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * 等差数列求和 初始值2 公差3 N=3n-1
+     *
+     * @param n
+     */
+    public void dengcha(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += 3 * i - 1;
+        }
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
         MathSolution mathSolution = new MathSolution();
-        mathSolution.qienisike(6);
+//        mathSolution.qienisike(6);
+//        String str = "10011000111";
+//        Optional<String> max = Arrays.stream(str.split("0")).max(Comparator.comparing(String::length));
+//        System.out.println(max.get().length());
+//        mathSolution.dengcha(275);
+        String a="I am a student";
+        System.out.println(new StringBuffer(a).reverse());
     }
 }
