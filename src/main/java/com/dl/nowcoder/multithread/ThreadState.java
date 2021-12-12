0c933a77cebf214db9dc85e1a67838390c592339
“DML” <dailin1357@gmail.com>
2021-12-12 23:43:50 +0800

    多线程-线程状态变化

--- src/main/java/com/dl/nowcoder/multithread/ThreadState.java
+++ src/main/java/com/dl/nowcoder/multithread/ThreadState.java
@@ -16,7 +16,7 @@ public class ThreadState
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //
         ThreadState threadState = new ThreadState();
         // threadState.stateNew();
@@ -26,7 +26,14 @@ public class ThreadState
 
     }
 
-    public void bolckTest() {
+    //线程由runnable状态进入waiting状态可通过Object.wait()，Thread.join()实现
+    //调⽤join()⽅法不会释放锁，会⼀直等待当前线程执⾏完毕（转换为TERMINATED状态）
+    //        t1.start();
+    //        t1.join();
+    //        t2.start();
+    //t1和t2启动之间插入t1.join()的话，main线程启动t1之后会等t1线程结束之后再执行t2.start(),
+
+    public void bolckTest() throws InterruptedException {
         Thread t1 = new Thread(new Runnable()
         {
             @Override
@@ -42,8 +49,10 @@ public class ThreadState
             }
         }, "t2");
         t1.start();
+//        Thread.sleep(100);
+        t1.join(3000L);
+//        t1.wait();
         t2.start();
-        Date start = new Date();
         System.out.println(t1.getName() + ": " + t1.getState());
         System.out.println(t2.getName() + ": " + t2.getState());
 
